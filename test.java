5de2ba110c8e22014ab3de6300114bc31c3d9592
15525072543 <50043340+15525072543@users.noreply.github.com>
2021-01-17 13:00:35 +0800

    删除文件
    删除练习文件

--- test.java
+++
@@ -1,6 +0,0 @@
-this is a test file,the people's name of develope is zhb; 
-this is the main branck update; create a new function;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-dddddddddddddd
-aaaaaaaaa
-cccccccc
